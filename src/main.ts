ac3785b1e777516fdadafdddb0630f21cbbb584c
DanNilloOli <danillooliveirasp@gmail.com>
2024-08-06 21:30:40 -0300

    Deploy_6

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00'; // Configuramos o fuso-horário
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Danillo Oliveira","https://www.linkedin.com/in/danillo-oliveira-76699515b/","danillooliveirasp@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe()); // Habilitamos o Validation Globalmente
+  process.env.TZ = '-03:00'; 
 
-  app.enableCors(); // Habilitamos requisições de outras origens (Servidores)
+  app.useGlobalPipes(new ValidationPipe()); 
 
-  await app.listen(4000);
+  app.enableCors(); 
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -23,6 +23,7 @@ describe('teste dos módulos usuario e auth (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
